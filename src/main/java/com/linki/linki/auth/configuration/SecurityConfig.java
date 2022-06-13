9f4778c82fb2c7d20ca7ff8801d6bceae7742b5c
yoonsukch@gmail.com <yoonsukch@gmail.com>
2022-06-13 22:06:32 +0900

    initial auth configuration

---
+++ src/main/java/com/linki/linki/auth/configuration/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.linki.linki.auth.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        return http
+                .httpBasic().disable() // api 서버기 때문에 로그인창이 필요 없음
+                .csrf().disable() // csrf 방어 필요 없음
+                .authorizeRequests()
+                .anyRequest().permitAll()
+//                                .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .and().build();
+
+
+    }
+}
