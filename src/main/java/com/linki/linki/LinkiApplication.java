c20301d1154d8de3a7919ca721a0c5b1bbe81a3c
yoonseokch <yoonsukch@gmail.com>
2022-06-13 15:18:46 +0900

    initial commit

---
+++ src/main/java/com/linki/linki/LinkiApplication.java
@@ -0,0 +1,13 @@
+package com.linki.linki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/linki/linki/LinkiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linki.linki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
