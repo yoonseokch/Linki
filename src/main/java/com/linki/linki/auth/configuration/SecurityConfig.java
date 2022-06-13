10cb392d179c315e9c6b94e04297fd5a8a47dcac
yoonsukch@gmail.com <yoonsukch@gmail.com>
2022-06-13 22:24:16 +0900

    add password encoder and password hash

--- src/main/java/com/linki/linki/auth/configuration/SecurityConfig.java
+++ src/main/java/com/linki/linki/auth/configuration/SecurityConfig.java
@@ -3,10 +3,18 @@ package com.linki.linki.auth.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
--- src/main/java/com/linki/linki/member/service/MemberService.java
+++ src/main/java/com/linki/linki/member/service/MemberService.java
@@ -5,6 +5,7 @@ import com.linki.linki.member.domain.MemberRole;
 import com.linki.linki.member.repository.MemberRepository;
 import com.linki.linki.member.vo.MemberCreateValues;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,11 +13,12 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public void addMember(MemberCreateValues values){
         memberRepository.save(Member.builder()
                 .loginID(values.getLoginID())
-                .passwordHash(values.getPassword()) // need to be hashed
+                .passwordHash(passwordEncoder.encode(values.getPassword())) // need to be hashed
                 .phoneNumber(values.getPhoneNumber())
                 .name(values.getName())
                 .memberRole(MemberRole.MEMBER)
