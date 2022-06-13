6bc9eee3fed4caa9ea526a4ae119600a7e4c7cc8
yoonsukch@gmail.com <yoonsukch@gmail.com>
2022-06-13 22:03:06 +0900

    ADD TODO into PasswordValidator

--- src/main/java/com/linki/linki/member/dto/validation/PasswordValidator.java
+++ src/main/java/com/linki/linki/member/dto/validation/PasswordValidator.java
@@ -10,6 +10,7 @@ public class PasswordValidator implements ConstraintValidator<Password,String> {
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         // TODO 패스워드 패턴 변경시 변경필요
+        // TODO 패스워드 체크할 건지 비지니스 로직 검증 필요
         Pattern pattern = Pattern.compile("[a-zA-Z_0-9]{10}+");
         Matcher matcher = pattern.matcher(value);
         return matcher.matches();
