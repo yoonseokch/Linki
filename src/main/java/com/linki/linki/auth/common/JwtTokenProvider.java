8d86472964d7ae160e4265aacb612818795f79ee
yoonsukch@gmail.com <yoonsukch@gmail.com>
2022-06-16 19:35:01 +0900

    시크릿 키와 제한시간 환경변수로 설정

--- src/main/java/com/linki/linki/auth/common/JwtTokenProvider.java
+++ src/main/java/com/linki/linki/auth/common/JwtTokenProvider.java
@@ -1,14 +1,22 @@
 package com.linki.linki.auth.common;
 
 import com.linki.linki.member.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtTokenProvider {
 
+    private final String secretKey;
+    private final long expireTimeInMS;
+
+    public JwtTokenProvider(@Value("${LINKY_SECRET_KEY}") String secretKey,@Value("${EXPIRE_LENGTH_IN_MILLISECONDS}") long expireTimeInMS) {
+        this.secretKey = secretKey;
+        this.expireTimeInMS = expireTimeInMS;
+    }
 
     public String createJwtToken(Member member) {
-
         return null;
     }
 }
