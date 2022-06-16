849152842d70ba1d599098d99504a2f63f70364d
yoonsukch@gmail.com <yoonsukch@gmail.com>
2022-06-16 18:56:40 +0900

    Jackson 라이브러리를 final 복구 및 noargsconstructor 추가

--- src/main/java/com/linki/linki/member/dto/MemberRegisterRequest.java
+++ src/main/java/com/linki/linki/member/dto/MemberRegisterRequest.java
@@ -4,33 +4,29 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.linki.linki.member.dto.validation.Password;
 import com.linki.linki.member.dto.validation.Phone;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
+@NoArgsConstructor(force = true)
+@RequiredArgsConstructor
 public class MemberRegisterRequest {
 
     @NotBlank(message = "이름은 공백일 수 없습니다.")
-    private String name;
+    private final String name;
 
     @NotBlank(message = "로그인 아이디는 공백일 수 없습니다.")
-    private String loginID;
+    private final String loginID;
 
     @Password
-    private String password;
+    private final String password;
 
     @NotBlank(message = "패스워드 체크는 공백일 수 없습니다.")
-    private String passwordCheck;
+    private final String passwordCheck;
 
     @Phone
-    private String phoneNumber;
+    private final String phoneNumber;
 
-    public MemberRegisterRequest(@NotBlank(message = "이름은 공백일 수 없습니다.") String name, @NotBlank(message = "로그인 아이디는 공백일 수 없습니다.") String loginID, String password, @NotBlank(message = "패스워드 체크는 공백일 수 없습니다.") String passwordCheck, String phoneNumber) {
-        this.name = name;
-        this.loginID = loginID;
-        this.password = password;
-        this.passwordCheck = passwordCheck;
-        this.phoneNumber = phoneNumber;
-    }
 }
