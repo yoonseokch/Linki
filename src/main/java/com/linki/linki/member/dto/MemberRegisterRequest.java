dac1abf0316cd78eba9f1f27db1c29e8ea40da00
yoonsukch@gmail.com <yoonsukch@gmail.com>
2022-06-16 17:42:50 +0900

    Jackson 라이브러리를 위해 final 필드 제거

--- src/main/java/com/linki/linki/member/dto/MemberRegisterRequest.java
+++ src/main/java/com/linki/linki/member/dto/MemberRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.linki.linki.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.linki.linki.member.dto.validation.Password;
 import com.linki.linki.member.dto.validation.Phone;
 import lombok.Getter;
@@ -8,22 +9,28 @@ import lombok.RequiredArgsConstructor;
 import javax.validation.constraints.NotBlank;
 
 @Getter
-@RequiredArgsConstructor
 public class MemberRegisterRequest {
 
     @NotBlank(message = "이름은 공백일 수 없습니다.")
-    private final String name;
+    private String name;
 
     @NotBlank(message = "로그인 아이디는 공백일 수 없습니다.")
-    private final String loginID;
+    private String loginID;
 
     @Password
-    private final String password;
+    private String password;
 
     @NotBlank(message = "패스워드 체크는 공백일 수 없습니다.")
-    private final String passwordCheck;
+    private String passwordCheck;
 
     @Phone
-    private final String phoneNumber;
+    private String phoneNumber;
 
+    public MemberRegisterRequest(@NotBlank(message = "이름은 공백일 수 없습니다.") String name, @NotBlank(message = "로그인 아이디는 공백일 수 없습니다.") String loginID, String password, @NotBlank(message = "패스워드 체크는 공백일 수 없습니다.") String passwordCheck, String phoneNumber) {
+        this.name = name;
+        this.loginID = loginID;
+        this.password = password;
+        this.passwordCheck = passwordCheck;
+        this.phoneNumber = phoneNumber;
+    }
 }
