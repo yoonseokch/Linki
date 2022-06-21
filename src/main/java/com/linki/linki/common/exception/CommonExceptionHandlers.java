59692c433cce3f0cfd569225bdeb6e7cd38a4274
yoonsukch@gmail.com <yoonsukch@gmail.com>
2022-06-21 15:52:02 +0900

    unvalid argument exception 핸들러 추가

--- src/main/java/com/linki/linki/common/exception/CommonExceptionHandlers.java
+++ src/main/java/com/linki/linki/common/exception/CommonExceptionHandlers.java
@@ -3,18 +3,38 @@ package com.linki.linki.common.exception;
 import com.linki.linki.member.exception.WrongPasswordException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 @ControllerAdvice
 public class CommonExceptionHandlers {
 
-    @ExceptionHandler({WrongPasswordException.class})
-    public ResponseEntity<Map<String, String>> unauthorizedHandler(Exception e) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(Collections.singletonMap("message", "잘못된 비밀번호입니다."));
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WrongPasswordException.class)
+    @ResponseBody
+    public Map<String, String> unauthorizedHandler(Exception e) {
+        return Collections.singletonMap("message", "잘못된 비밀번호입니다.");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public Map<String, String> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
