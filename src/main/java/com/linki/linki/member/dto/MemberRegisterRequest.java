73ac78caaf660207703aad8129ff6589230a3be7
yoonsukch@gmail.com <yoonsukch@gmail.com>
2022-06-22 17:59:10 +0900

    password 체크 프론트에서 하게 수정

--- src/main/java/com/linki/linki/member/dto/MemberRegisterRequest.java
+++ src/main/java/com/linki/linki/member/dto/MemberRegisterRequest.java
@@ -22,9 +22,6 @@ public class MemberRegisterRequest {
     @Pattern(regexp = "[a-zA-Z_0-9]{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자를 사용하세요.")
     private final String password;
 
-    @NotBlank(message = "패스워드 체크는 공백일 수 없습니다.")
-    private final String passwordCheck;
-
     @NotBlank
     @Pattern(regexp = "\\d{3}-\\d{4}-\\d{4}", message = "핸드폰 번호가 형식에 맞지 않습니다.")
     private final String phoneNumber;
